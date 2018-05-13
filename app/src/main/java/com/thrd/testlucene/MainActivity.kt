9f08a78bcfd3cf5c012cf6f2c2bec55bf39ed6c3
blackhole <raggamuffin.inda.shell@gmail.com>
2018-05-14 00:23:43 +0300

    Add UI

--- app/src/main/java/com/thrd/testlucene/MainActivity.kt
+++ app/src/main/java/com/thrd/testlucene/MainActivity.kt
@@ -1,12 +1,57 @@
 package com.thrd.testlucene
 
-import android.support.v7.app.AppCompatActivity
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.Menu
+import android.widget.SearchView
+
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        private const val TAG = "MainActivity"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        handleIntent(intent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the options menu from XML
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+
+        // Get the SearchView and set the searchable configuration
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.menu_search).actionView as SearchView
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.setIconifiedByDefault(false) // Do not iconify the widget; expand it by default
+
+        return true
+    }
+
+
+    override fun onNewIntent(intent: Intent) {
+        handleIntent(intent)
+    }
+
+    private fun handleIntent( intent: Intent) {
+
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY)
+            //use the query to search your data somehow
+            doSearch(query)
+        }
+    }
+    private fun doSearch(query: String?) {
+        Log.d(TAG, "doSearch(): $query")
     }
 }
